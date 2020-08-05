23147599e8201a965ff265042aaddba6c0936559
wangsq8 <wangsq8@ziroom.com>
2020-08-05 23:10:00 +0800

    文章网站示例

---
+++ demo2/src/main/java/com/asq/demo2/ArticleVote.java
@@ -0,0 +1,13 @@
+package com.asq.demo2;
+
+public class ArticleVote {
+
+    private int ONE_WEEK_IN_SECONDS=7*86400;
+
+    private int VOTE_SCORE=432;
+
+    public void vote(){
+
+    }
+
+}
---
+++ demo2/src/main/java/com/asq/demo2/JedisUtils.java
@@ -0,0 +1,10 @@
+package com.asq.demo2;
+
+public class JedisUtils {
+
+    private Jedis jedis;
+
+    public static Jedis getJedis(){
+
+    }
+}
